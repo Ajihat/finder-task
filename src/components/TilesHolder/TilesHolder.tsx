7b2b601da92af23c22707526044f308b9ce83756
Dawid Lechowski <david.lechowski@gmail.com>
2023-10-14 13:59:24 +0200

    README.md update

--- src/components/TilesHolder/TilesHolder.tsx
+++ src/components/TilesHolder/TilesHolder.tsx
@@ -3,5 +3,5 @@ import { TilesHolderProps } from './TilesHolder.types';
 import styles from './TilesHolder.module.css';
 
 export const TilesHolder = ({ children }: TilesHolderProps) => {
-  return <div className={styles.holder}>{children}</div>;
+  return <ul className={styles.holder}>{children}</ul>;
 };
